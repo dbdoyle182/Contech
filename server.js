1d3a7405deb5b6691cc59bc013fd8b9943fbdc2e
brad ferrell <ferrellbh@gmail.com>
2018-04-27 16:58:40 -0400

    .

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static('./server/static/'))
-app.use(express.static("/client/dist/"));
+app.use(express.static("./client/public/"));
 // Add routes, both API and view
 
-
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dictionary");
 
 // start the server
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:3001 or http://127.0.0.1:3001');
+  console.log(
+    "Server is running on http://localhost:3001 or http://127.0.0.1:3001"
+  );
 });
