3d1e93b6fa4b851e2c92b08f9ec2b8229010f009
brad ferrell <ferrellbh@gmail.com>
2018-04-29 19:43:03 -0400

    added some initial routes in term.js controller. also updated term model with property word to exit redundany in using Term as model name and prop name

--- routes/terms.js
+++ routes/terms.js
@@ -5,10 +5,31 @@ const router = express.Router();
 //----Create Routes----
 
 //Get Route for all terms
+router.get("/term/all", (req, res) => {
+  db.Term.find({})
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 //Get Route for term by user input search
 
 //Get Route for term by alphabet letter search
+router.get("/term/:letter", (req, res) => {
+  const letter = req.params.letter;
+  db.Term.find({
+    word: /^letter/
+  })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 //Get Route for term by filter search
 
