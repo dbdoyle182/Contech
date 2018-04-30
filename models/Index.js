97df770c2506eb61e0825710d1d1e124fc516826
brad ferrell <ferellbh@gmail.com>
2018-04-30 12:46:36 -0400

    updated index.js in models so that connect was a part of the export object, not the only thing in it

--- models/Index.js
+++ models/Index.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
-//Exports and connects the models 
-module.exports.connect = (uri) => {
-  mongoose.connect(uri);
+const mongoose = require("mongoose");
+//Exports and connects the models
+module.exports = {
+  connect: uri => {
+    mongoose.connect(uri);
 
-  mongoose.Promise = global.Promise;
+    mongoose.Promise = global.Promise;
 
-
-  mongoose.connection.on('error', (err) => {
-    console.error(`Mongoose connection error: ${err}`);
-    process.exit(1);
-  });
-
-
-  Term: require("./Term");
-  Quiz: require("./Quiz");
-  User: require("./User");
-  Comment: require("./Comment");
+    mongoose.connection.on("error", err => {
+      console.error(`Mongoose connection error: ${err}`);
+      process.exit(1);
+    });
+  },
+  Term: require("./Term"),
+  Quiz: require("./Quiz"),
+  User: require("./User"),
+  Comment: require("./Comment")
 };
--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const passport = require('passport');
-const config = require('./config');
+const passport = require("passport");
+const config = require("./config");
 
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-require('./models').connect(process.env.MONGODB_URI || config.dbUri);
+const db = require("./models");
+db.connect(process.env.MONGODB_URI || config.dbUri);
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(morgan("dev"));
 // Add routes, both API and view
 
 app.use(passport.initialize());
 
-const localSignupStrategy = require('./authentication/passport/local-signup');
-const localLoginStrategy = require('./authentication/passport/local-login');
-passport.use('local-signup', localSignupStrategy);
-passport.use('local-login', localLoginStrategy);
+const localSignupStrategy = require("./authentication/passport/local-signup");
+const localLoginStrategy = require("./authentication/passport/local-login");
+passport.use("local-signup", localSignupStrategy);
+passport.use("local-login", localLoginStrategy);
 
-const authCheckMiddleware = require('./authentication/middleware/auth-check');
-app.use('/api', authCheckMiddleware);
+const authCheckMiddleware = require("./authentication/middleware/auth-check");
+app.use("/api", authCheckMiddleware);
 
-
-const authRoutes = require('./routes/auth');
-const apiRoutes = require('./routes/api');
-app.use('/auth', authRoutes);
-app.use('/api', apiRoutes);
+const authRoutes = require("./routes/auth");
+const apiRoutes = require("./routes/api");
+app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
 
 app.use(express.static("./client/public/"));
 
