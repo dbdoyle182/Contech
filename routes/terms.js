dbd7f6d78eeb35306fddc177fa06b44466daaa1f
Brad Ferrell <33673313+bradbotcode@users.noreply.github.com>
2018-04-30 22:28:29 -0400

    Update terms.js

--- routes/terms.js
+++ routes/terms.js
@@ -16,7 +16,7 @@ router.get("/term/all", (req, res) => {
 });
 
 //Get Route for term by user input search
-router.get("search/:input", (req, res) => {
+router.get("/search/:input", (req, res) => {
   console.log(req.query);
   console.log(req.params.input);
   const input = req.params.input;
@@ -48,7 +48,7 @@ router.get("/search/:letter", (req, res) => {
 });
 
 //Get Route for term by filter search
-router.get("search/:filter", (req, res) => {
+router.get("/search/:filter", (req, res) => {
   console.log(req.query);
   console.log(req.params.filter);
   const filter = req.params.filter;
