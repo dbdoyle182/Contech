35fc83a2595506814b4eeb0c555f0ff8f0ba7178
brad ferrell <ferrellbh@gmail.com>
2018-04-30 20:40:17 -0400

    new post route for new comment on a term page

--- routes/comments.js
+++ routes/comments.js
@@ -4,8 +4,22 @@ const router = express.Router();
 
 //----Create Routes----
 
-//Get Route for saved comments on a term
-
 //Post Route for adding a new comment on a term
+router.post("/newComment/:id", (req, res) => {
+  db.Comment.create(req.body)
+    .then(dbComm => {
+      return db.Term.findOneAndUpdate(
+        { _id: req.params.id },
+        { comment: comm._id },
+        { new: true }
+      );
+    })
+    .then(dbTerm => {
+      res.json(dbTerm);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
