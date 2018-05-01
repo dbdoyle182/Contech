d4742730e12ba679da191099d3483310c717909e
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-30 21:49:40 -0400

    add term form

--- client/src/containers/TermFormPage.js
+++ client/src/containers/TermFormPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import TermForm from '../components/TermForm';
+import { Card, TextField, RaisedButton, MenuItem, SelectField } from 'material-ui'
+
+const filtertags = ['Git','Framework','Library','Language','Data','Software','World Wide Web', 'Language Feature', 'Miscellaneous','Frontend','Backend'];
 
 class TermFormPage extends Component {
 
@@ -20,8 +22,10 @@ class TermFormPage extends Component {
 
         this.processForm = this.processForm.bind(this);
         this.changeWord = this.changeWord.bind(this);
-        this.handleChecks = this.handleChecks.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.menuItems = this.menuItems.bind(this);
     }
+    
 
     changeWord(event) {
         const field = event.target.name;
@@ -34,9 +38,14 @@ class TermFormPage extends Component {
         })
     }
 
-    handleChecks(event) {
-        
-    }    
+    handleChange(event, index, values) {
+
+        this.setState({
+            tag: values
+        })
+    }
+
+
 
     processForm(event) {
         event.preventDefault();
@@ -45,15 +54,87 @@ class TermFormPage extends Component {
         console.log(this.state.word)
     }
 
-    render() {
-        return (
-            <TermForm
-                onSubmit={this.processForm}
-                onChange={this.changeWord}
-                errors={this.state.errors}
-                word={this.state.word}
-                wordlibrary={['word','word','word']}
+    menuItems(values) {
+        return filtertags.map((tag) => (
+            <MenuItem
+                key={tag}
+                insetChildren={true}
+                checked={values && values.indexOf(tag) > -1}
+                value={tag}
+                primaryText={tag}
             />
+        ));
+    }
+
+    render() {
+        const {tags} = this.state.word.tags
+        return (
+            
+            <Card className='container'>
+                <form action='/' onSubmit={this.processForm}>
+                <h2 className='card-heading'>Add a Term</h2>
+
+                {this.state.errors.summary && <p className='error-message'>{this.state.errors.summary}</p>}
+
+                <div className='field-line'>
+                    <TextField
+                        floatingLabelText='Word'
+                        name='word'
+                        errorText={this.state.errors.term}
+                        onChange={this.changeWord}
+                        value={this.state.word.word}
+                    />
+                </div>
+
+                <div className='field-line'>
+                    <TextField  
+                        floatingLabelText='Summary'
+                        name='summary'
+                        multiLine={true}
+                        rows={2}
+                        rowsMax={4}
+                        errorText={this.state.errors.summary}
+                        value={this.state.word.summary}
+                        onChange={this.changeWord}
+                    />
+                </div>
+
+                <div className='field-line'>
+                    <TextField  
+                        floatingLabelText='Definition'
+                        name='definition'
+                        multiLine={true}
+                        rows={2}
+                        rowsMax={7}
+                        errorText={this.state.errors.definition}
+                        value={this.state.word.definition}
+                        onChange={this.changeWord}
+                    />
+                </div>
+
+                <div className='field-line'>
+                    <SelectField
+                        multiple={true}
+                        hintText='Select tags'
+                        value={tags}
+                        onChange={this.handleChange}
+                    >
+                        {this.menuItems(tags)}
+                    </SelectField>
+                </div>
+
+            <div className='field-line'>
+                
+            </div>
+
+            <div className='field-line'>
+            </div>
+
+            <div className='button-line'>
+                <RaisedButton type='submit' label='Add this term' primary/>
+            </div>
+        </form>
+    </Card>
         )
     }
 }
