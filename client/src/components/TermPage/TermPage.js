d57a746e05e886e8be0079920091b2d1e1b74856
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-02 17:50:25 -0400

    related buttons functioning properly

--- client/src/components/TermPage/TermPage.js
+++ client/src/components/TermPage/TermPage.js
@@ -30,17 +30,17 @@ class TermPage extends Component {
             .catch(err => console.log(err));
     }
 
-    componentDidUpdate() {
-        axios.get(this.props.match.params.input)
-            .then(res => {
-                this.setState({
-                    word: res.data[0],
-                    comments: res.data[0].comments
-                })
-                console.log(this.state.word)
-            })
-            .catch(err => console.log(err));
-    }
+    // componentDidUpdate() {
+    //     axios.get(this.props.match.params.input)
+    //         .then(res => {
+    //             this.setState({
+    //                 word: res.data[0],
+    //                 comments: res.data[0].comments
+    //             })
+    //             console.log(this.state.word)
+    //         })
+    //         .catch(err => console.log(err));
+    // }
 
 
 
@@ -56,7 +56,7 @@ class TermPage extends Component {
                     </div>
                     <div className="TermPage-flex-container25">
                         <TermTags tags1={this.state.word.tags1} tags2={this.state.word.tags2}/>
-                        <TermRelevant relevant1={this.state.word.related1} relevant2={this.state.word.related2}/>
+                        <TermRelevant relevant1={this.state.word.related1} relevant2={this.state.word.related2} />
                     </div>
                 </div>
                 <TermExtended word={this.state.word.word} definition={this.state.word.definition}/>
--- client/src/components/TermRelevant/TermRelevant.js
+++ client/src/components/TermRelevant/TermRelevant.js
@@ -6,7 +6,7 @@ const TermRelevant = props => (
     
 
     <div className="termRelevant">
-        <button><Link to={'/search/' + props.relevant1} replace>{props.relevant1}</Link></button>
+        <button onClick={() => {window.location.reload()}}><Link to={'/search/' + props.relevant1} replace>{props.relevant1}</Link></button>
         <button><Link to={'/search/' + props.relevant2} replace>{props.relevant2}</Link></button>
     </div>
 );
