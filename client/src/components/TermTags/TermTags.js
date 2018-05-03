53c6e532bd78d3424b46a6801e8acb19c09d8e65
unknown <bradleyaw@gmail.com>
2018-05-03 12:12:04 -0400

    minor term page changes

--- client/src/components/TermTags/TermTags.js
+++ client/src/components/TermTags/TermTags.js
@@ -5,8 +5,8 @@ import "./TermTags.css";
 const TermTags = props => (
     <div className="termTags">
         <h4>Tags</h4>
-        {props.tags1 && <button><Link to={'/search/' + props.tags1}>{props.tags1}</Link></button>}
-        {props.tags2 && <button><Link to={'/search/' + props.tags2}>{props.tags2}</Link></button>}
+        {props.tags1 && <button>{props.tags1}</button>}
+        {props.tags2 && <button>{props.tags2}</button>}
     </div>
 );
 
