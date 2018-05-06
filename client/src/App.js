940b7699078904e23a342ab653668992a99942fb
brad ferrell <ferrellbh@gmail.com>
2018-05-05 21:27:05 -0400

    working 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import HomePage from './components/HomePage';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LoginPage from './containers/LoginPage.js';
-import SignUpPage from './containers/SignUpPage.js';
-import LogoutFunction from './containers/LogoutFunction';
-import Navbar from './components/Navbar.js';
-import TermPage from './components/TermPage';
-import TermFormPage from './containers/TermFormPage';
+import React, { Component } from "react";
+import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginPage from "./containers/LoginPage.js";
+import SignUpPage from "./containers/SignUpPage.js";
+import LogoutFunction from "./containers/LogoutFunction";
+import Navbar from "./components/Navbar.js";
+import TermPage from "./components/TermPage";
+import TermFormPage from "./containers/TermFormPage";
+import NotFound from "./components/404";
 
-class App extends Component { 
-
+class App extends Component {
   render() {
     return (
       <Router>
         <div>
-            <Navbar />
-            <Switch>
-              <Route exact path='/' component={HomePage} />
-              <Route path='/login' component={LoginPage} />
-              <Route path='/signup' component={SignUpPage} />
-              <Route path='/logout' component={LogoutFunction} />
-              <Route path='/addterm' component={TermFormPage} />
-              <Route path='/search/:input' component={TermPage} />
-
-            </Switch>  
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/signup" component={SignUpPage} />
+            <Route path="/logout" component={LogoutFunction} />
+            <Route path="/addterm" component={TermFormPage} />
+            <Route path="/search/:input" component={TermPage} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </Router>
-    )  
-  }    
+    );
+  }
 }
 
 export default App;
