f37e679bd60709fb13bdcdb2ca2abd7105981913
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-06 19:31:41 -0400

    eliminated the loop

--- client/src/components/TermComments/TermComments.js
+++ client/src/components/TermComments/TermComments.js
@@ -9,96 +9,14 @@ class TermComments extends Component {
     constructor(props, context) {
         super(props, context);
 
-        this.state = {
-            comment: '',
-            user: {},
-            update: false,
-            commentEdit: ''
-        }
-
-        this.updateComment = this.updateComment.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
     }
     
-    componentDidMount() {
-        
-        if(Auth.isUserAuthenticated()) {
-            const username = localStorage.getItem('username')
-            axios.get(`/user/${username}`)
-                .then(res => {
-                    this.setState({ user: res.data[0]})
-                })
-                .catch(err => console.log(err))
-        }
-        
-    }
+    
 
     
     
 
-    onSubmit(event) {
-        event.preventDefault();
-        const postRoute = '/newComment/' + this.props.id
-        
 
-        axios.post(postRoute, ({
-            body: this.state.comment,
-            author: this.state.user._id,
-            authorName: this.state.user.username
-        })).then(res => {
-            console.log('Comment posted')
-            this.setState({
-                comment: ''
-            })
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-
-    handleChange(event) {
-        const { name, value } = event.target;
-
-        this.setState({
-            [name]: value
-            });
-
-        
-    }
-
-    deleteComment(id) {
-        axios.delete('/newComment/' + id)
-            .then(res => {
-                console.log('Your comment has been deleted')
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-    updateComment(id, comment) {
-        if(this.state.update === false) {
-            this.setState({
-                update: true,
-                comment: comment,
-                commentEdit: id
-            })
-        }
-        if(this.state.update === true) {
-            const postRoute = '/updateComment/' + id;
-            axios.post(postRoute, ({
-                body: this.state.comment
-            }))
-                .then(res => {
-                    console.log('Your comment has been updated')
-                    this.setState({
-                        update: false
-                    })
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        }
-    }
     
     render() {
         return (
@@ -109,14 +27,14 @@ class TermComments extends Component {
                     <div key={comment._id}>
                         <div>{comment.authorName}</div>
                         <div>{moment(comment.createdAt).format('dddd-MMM-YYYY  HH:mm')}</div>
-                        {this.state.update === true && this.state.commentEdit === comment._id ? <textarea onChange={this.handleChange.bind(this)} value={this.state.comment} name='comment'></textarea>
+                        {this.props.update === true && this.props.commentId === comment._id ? <textarea onChange={this.props.handleChange} value={this.props.commentEdit} name='commentEdit'></textarea>
                         : <div style={{ whiteSpace:'pre-wrap', textAlign: 'left'}} >{comment.body}</div>}
                         <div>
-                            {(this.state.update === false && this.state.user.username === comment.authorName) && <button onClick={() => this.updateComment(comment._id, comment.body)}>Update</button>}
-                            {this.state.user.admin ? <button onClick={() => this.deleteComment(comment._id)}>Delete</button>
-                            : this.state.user.username === comment.authorName && <button onClick={() => this.deleteComment(comment._id)}>Delete</button>
+                            {(this.props.update === false && this.props.username === comment.authorName) && <button onClick={() => this.props.updateComment(comment._id, comment.body)}>Update</button>}
+                            {this.props.admin ? <button onClick={() => this.props.deleteComment(comment._id)}>Delete</button>
+                            : this.props.username === comment.authorName && <button onClick={() => this.props.deleteComment(comment._id)}>Delete</button>
                         }
-                            {(this.state.update === true && this.state.commentEdit === comment._id) && <button onClick={() => this.updateComment(comment._id, this.state.comment)}>Submit</button>}
+                            {(this.props.update === true && this.props.commentId === comment._id) && <button onClick={() => this.props.updateComment(comment._id, this.props.commentEdit)}>Submit</button>}
                         </div>
                     </div>
                     )
@@ -124,9 +42,9 @@ class TermComments extends Component {
                     <h4>FORM</h4>
                 {Auth.isUserAuthenticated() ?
                     (<div>
-                        <h2>Commenting as: {this.state.user.username}</h2>
-                        <form onSubmit={this.onSubmit.bind(this)} >
-                            <textarea style={{ width:'80%', height:'100px'}}name='comment' type='text' value={this.state.title} onChange={this.handleChange.bind(this)} />
+                        <h2>Commenting as: {this.props.username}</h2>
+                        <form onSubmit={this.props.commentSubmit} >
+                            <textarea style={{ width:'80%', height:'100px'}} name='comment' type='text' value={this.props.comment} onChange={this.props.handleChange} />
                             <button type='submit'>Submit</button>
                         </form>
                     </div>) : (
--- client/src/components/TermPage/TermPage.js
+++ client/src/components/TermPage/TermPage.js
@@ -20,25 +20,36 @@ class TermPage extends Component {
             comments: [],
             browseResults: [],
             editWord: false,
-            user: {}
+            comment: '',
+            commentEdit: '',
+            user: {},
+            update: false,
+            commentId: ''
         }
 
         this.filterHandler = this.filterHandler.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.updateTerm = this.updateTerm.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.commentChange = this.commentChange.bind(this)
+        this.commentSubmit = this.commentSubmit.bind(this)
+        this.deleteComment = this.deleteComment.bind(this)
+        this.updateComment = this.updateComment.bind(this)
     }
 
-    componentDidMount() {
+    refreshWordInfo() {
         axios.get(this.props.match.params.input)
-            .then(res => {
-                this.setState({
-                    word: res.data[0],
-                    comments: res.data[0].comments
-                })
-                
+        .then(res => {
+            this.setState({
+                word: res.data[0],
+                comments: res.data[0].comments
             })
-            .catch(err => console.log(err));
+            
+        })
+        .catch(err => console.log(err));
+    }
+    componentDidMount() {
+        this.refreshWordInfo()
 
         if(Auth.isUserAuthenticated()) {
             const username = localStorage.getItem('username')
@@ -51,16 +62,16 @@ class TermPage extends Component {
         }
     }
 
-    componentDidUpdate() {
-        axios.get(this.props.match.params.input)
-            .then(res => {
-                this.setState({
-                    comments: res.data[0].comments
-                })
+    // componentDidUpdate() {
+    //     axios.get(this.props.match.params.input)
+    //         .then(res => {
+    //             this.setState({
+    //                 comments: res.data[0].comments
+    //             })
                 
-            })
-            .catch(err => console.log(err));
-    }
+    //         })
+    //         .catch(err => console.log(err));
+    // }
 
     filterHandler(filter) {
         axios.get('/filterBy/' + filter)
@@ -114,6 +125,73 @@ class TermPage extends Component {
             })
     }
 
+    commentSubmit(event) {
+        event.preventDefault();
+        const postRoute = '/newComment/' + this.state.word._id
+        
+
+        axios.post(postRoute, ({
+            body: this.state.comment,
+            author: this.state.user._id,
+            authorName: this.state.user.username
+        })).then(res => {
+            console.log('Comment posted')
+            this.setState({
+                comment: ''
+            })
+            this.refreshWordInfo()
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    commentChange(event) {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+            });
+
+        
+    }
+
+    deleteComment(id) {
+        axios.delete('/newComment/' + id)
+            .then(res => {
+                console.log('Your comment has been deleted')
+                this.refreshWordInfo()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    updateComment(id, comment) {
+        if(this.state.update === false) {
+            this.setState({
+                update: true,
+                commentEdit: comment,
+                commentId: id
+            })
+        }
+        if(this.state.update === true) {
+            const postRoute = '/updateComment/' + id;
+            axios.post(postRoute, ({
+                body: this.state.commentEdit
+            }))
+                .then(res => {
+                    console.log('Your comment has been updated')
+                    this.setState({
+                        update: false
+                    })
+                    this.refreshWordInfo();
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }
+
     
 
 
@@ -147,7 +225,20 @@ class TermPage extends Component {
                     handleChange={this.handleChange} 
                     editTerm={this.state.editWord}
                 />
-                <TermComments id={this.state.word._id} comments={this.state.comments}/>
+                <TermComments 
+                    id={this.state.word._id} 
+                    comments={this.state.comments}
+                    update={this.state.update}
+                    commentId={this.state.commentId}
+                    handleChange={this.commentChange}
+                    comment={this.state.comment}
+                    updateComment={this.updateComment}
+                    username={this.state.user.username}
+                    admin={this.state.user.admin}
+                    deleteComment={this.deleteComment}
+                    commentSubmit={this.commentSubmit}
+                    commentEdit={this.state.commentEdit}
+                    />
             </div>
         </Card>
     )}
