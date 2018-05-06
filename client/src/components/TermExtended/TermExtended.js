2e6c3affaec854371985747a5690ee6dd5594b28
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-06 19:55:00 -0400

    minor style to textarea

--- client/src/components/TermExtended/TermExtended.js
+++ client/src/components/TermExtended/TermExtended.js
@@ -4,7 +4,7 @@ import "./TermExtended.css";
 const TermExtended = props => (
     <div className="termExtended">
             <h3>Extended Definition: {props.word}</h3>
-            {props.editTerm ? <textarea onChange={props.handleChange} value={props.definition} name='definition'></textarea> 
+            {props.editTerm ? <textarea style={{ width: '80%', height: '100px'}} onChange={props.handleChange} value={props.definition} name='definition'></textarea> 
             : <p>{props.definition}</p>}
     </div>
 );
--- client/src/components/TermMain/TermMain.js
+++ client/src/components/TermMain/TermMain.js
@@ -4,7 +4,7 @@ import "./TermMain.css";
 const TermMain = (props) => (
     <div className="termMain">
             <h2>{props.word}</h2>
-            {props.edit ? <textarea onChange={props.handleChange} value={props.summary} name='summary'></textarea>
+            {props.edit ? <textarea style={{ width: '80%', height: '100px'}} onChange={props.handleChange} value={props.summary} name='summary'></textarea>
             : <p>{props.summary}</p>}
             {props.admin && props.edit === false && <button onClick={props.editWord}>Edit Term</button>}
             {props.admin && props.edit && <button onClick={props.handleSubmit}>Submit</button>}
