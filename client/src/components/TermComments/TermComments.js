9331e8787e8977ce368d98ed1270de5961c3e7ed
unknown <bradleyaw@gmail.com>
2018-05-07 12:38:04 -0400

    comment and term page styling changes

--- client/src/components/TermComments/TermComments.js
+++ client/src/components/TermComments/TermComments.js
@@ -8,39 +8,39 @@ import moment from 'moment';
 const TermComments = props =>  (
 
     <div className="termComments">
-            <h3>Comments</h3>
-                {(this.props.comments).map(comment => {
-                    
-                    return (
-                    <div className="commentContainer" key={comment._id}>
-                        <div className="commentAuthor">{comment.authorName}</div>
-                        {this.state.update === true && this.state.commentEdit === comment._id ? <textarea className="editBox" onChange={this.handleChange.bind(this)} value={this.state.comment} name='comment'></textarea>
-                        : <div className="commentBody">
-                            <div className="commentBar">{moment(comment.createdAt).format('MMMM Do YYYY  hh:mma')} {this.state.update === false && <img className="commentIcon"  onClick={() => this.updateComment(comment._id, comment.body)} src='/images/pencil.png' alt="edit" />} <img onClick={() => this.deleteComment(comment._id)} className="commentIcon" src='/images/trashcan.png' alt="delete" /></div>
-                            <div>
-                                {comment.body}
-                            </div>
-                        </div>}
-                        {this.state.user.username === comment.authorName &&
-                        <div>
-                            {(this.state.update === true && this.state.commentEdit === comment._id) && <button className="deleteComment" onClick={() => this.updateComment(comment._id, this.state.comment)}>Done</button>}
-                        </div>}
+    <h3>Comments</h3>
+        {(props.comments).map(comment => {
+            
+            return (
+            <div className="commentContainer" key={comment._id}>
+                <div className="commentAuthor">{comment.authorName}</div>
+                {props.update === true && props.commentId === comment._id ? <textarea className="editBox" onChange={props.handleChange} value={props.commentEdit} name='commentEdit'></textarea>
+                : <div className="commentBody">
+                    <div className="commentBar">{moment(comment.createdAt).format('MMMM Do YYYY  hh:mma')} {props.update === false && <img className="commentIcon"  onClick={() => props.updateComment(comment._id, comment.body)} src='./images/pencil.png' alt="edit" />} <img onClick={() => props.deleteComment(comment._id)} className="commentIcon" src='./images/trashcan.png' alt="delete" /></div>
+                    <div>
+                        {comment.body}
                     </div>
-                    )
-                })}
-                {Auth.isUserAuthenticated() ?
-                    (
-                        <div className="addCommentContainer">
-                            <h4>Join the conversation</h4>
-                            <span className="commenting">Commenting as: {this.state.user.username}</span>
-                            <form onSubmit={this.onSubmit.bind(this)} >
-                                <textarea className="newComment" name='comment' type='text' value={this.state.title} onChange={this.handleChange.bind(this)} />
-                                <div className="buttonContainer"><button className="submitComment" type='submit'>Submit</button></div>
-                            </form>
-                        </div>) : (
-                        <div><Link to='/login'>Log in</Link> or <Link to='/signup'>Sign up</Link> to post a comment!</div>
-                    )
-                }
+                </div>}
+                {props.username === comment.authorName &&
+                <div>
+                    {(props.update === true && props.commentId === comment._id) && <button className="deleteComment" onClick={() => props.updateComment(comment._id, props.commentEdit)}>Done</button>}
+                </div>}
+            </div>
+            )
+        })}
+        {Auth.isUserAuthenticated() ?
+            (
+                <div className="addCommentContainer">
+                    <h4>Join the conversation</h4>
+                    <span className="commenting">Commenting as: {props.username}</span>
+                    <form onSubmit={props.commentSubmit} >
+                        <textarea className="newComment" name='comment' type='text' value={props.comment} onChange={props.handleChange} />
+                        <div className="buttonContainer"><button className="submitComment" type='submit'>Submit</button></div>
+                    </form>
+                </div>) : (
+                <div><Link to='/login'>Log in</Link> or <Link to='/signup'>Sign up</Link> to post a comment!</div>
+            )
+        }
     </div>
 )
 export default TermComments;
