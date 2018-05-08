72420706e53cc3f7ca98b6be7d783751553e1daf
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-07 21:58:48 -0400

    add tap events

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import App from "./App";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import registerServiceWorker from "./registerServiceWorker";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+injectTapEventPlugin();
+
+
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={getMuiTheme()}>
